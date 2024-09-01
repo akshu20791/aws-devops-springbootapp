255c437d3dcecf8ff261e742ecd33786a34666b0
ag <ag@gmail.com>
2024-09-01 15:11:23 +0530

    my first commit

---
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -0,0 +1,19 @@
+package com.example.springbootawsdeploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+    @GetMapping("/data")
+    public String getData() {
+        return "First message from AWS Ecs";
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Second message from AWS Ecs";
+    }
+}
